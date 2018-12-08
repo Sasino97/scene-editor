6ef3b6e59bd5318a7d574a44deab1addd9fced92
Salvatore Strazzullo <sasinosoft@gmail.com>
2018-12-08 13:40:49 +0100

    Various.

--- Sasinosoft.SampMapEditor/World/GameObject.cs
+++ Sasinosoft.SampMapEditor/World/GameObject.cs
@@ -1,9 +1,5 @@
 ﻿using Sasinosoft.SampMapEditor.RenderWare;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Sasinosoft.SampMapEditor.World
 {
