dafc1bf6cc6417a5e705e9b48f43be87215bc060
SaSiNO97 <SaSiNO97@localhost>
2018-12-08 13:40:49 +0100

    Various.

---
+++ Sasinosoft.SampMapEditor/IPL/IPLElement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sasinosoft.SampMapEditor.IPL
+{
+    public class IPLElement
+    {
+        public int ModelID { get; set; }
+        public string ModelName { get; set; }
+        public int Unknown1 { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        public double RX { get; set; }
+        public double RY { get; set; }
+        public double RZ { get; set; }
+        public double Scale { get; set; }
+        public int Unknown2 { get; set; }
+    }
+}
